a8a19896650ab740a5f8348cd6435c28544c8864
Manju Sagar <manjusagar@railsfactory.org>
2014-07-02 09:28:11 +0530

    operations on numbers

---
+++ aroperation/ar.rb
@@ -0,0 +1,9 @@
+puts "enter the two integers"
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+puts "addition of two numbers is #{a+b}"
+puts "subtraction of two numbers is #{a-b}"
+puts "division of two numbers is #{a/b}"
+puts "multiplication of two numbers is #{a*b}"
+
+
