5070e9d2907a328bb60738c59361ae06745b0f4b
Manju Sagar <manjusagar@railsfactory.org>
2014-07-03 12:59:23 +0530

    removal of duplications

--- Basiccalculator/calculator.rb
+++ Basiccalculator/calculator.rb
@@ -1,31 +1,31 @@
+$i = 0
 class Calculator
-   
     def initialize(num)
-     @ini = num.to_i
+     @a = num.to_i
     end
-   
+    $i = @a
 
      def add(num1)
-      puts  @ini + num1
-    
+     $i =  $i + num1
+      puts $i
      end
 
      
       def sub(num2)
-     puts @ini - num2
-   
+      $i =  $i - num2
+      puts $i
       end
 
 
       def mul(num3)
-       puts @ini * num3
-       
+       $i =  $i * num3
+       puts $i
       end
 
 
       def div(num4)
-       puts @ini / num4
-       
+       $i = $i / num4
+        puts $i
      end
 end
 
