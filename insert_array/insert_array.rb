aba3ad5bbec8ccf3db215cde95210448c9f13e25
Manju Sagar <manjusagar@railsfactory.org>
2014-07-04 18:01:40 +0530

    insert into array

---
+++ insert_array/insert_array.rb
@@ -0,0 +1,15 @@
+puts "enter the number of elements the array"
+num = gets.chomp.to_i
+arr = [] 
+arr1 = []
+puts "enter the elements one by one "
+  for i in 1..num
+   arr[i] = gets.chomp.to_i
+  end
+puts "enter the location where you want to insert array"
+  num1 = gets.chomp.to_i
+
+puts "enter the element you want to insert "
+  ele = gets.chomp.to_i
+
+ arr
