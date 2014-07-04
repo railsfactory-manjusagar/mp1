f9a2e9208c8053e523ec0c19639e58bc67765979
Manju Sagar <manjusagar@railsfactory.org>
2014-07-04 17:52:58 +0530

    matching of strings

---
+++ string_match/strmatch.rb
@@ -0,0 +1,11 @@
+a=Array.new
+b=Array.new
+puts"enter the 1st string"
+a=gets.chomp.to_s
+puts"enter the 2nd string"
+b=gets.chomp.to_s
+if a.eql? b
+puts"entered strings are equal"
+else
+puts"entered strings are not equal"
+end
