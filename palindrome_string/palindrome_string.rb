5e5503f2e352dd408ae37d276e5d4f9c60992308
Manju Sagar <manjusagar@railsfactory.org>
2014-07-07 14:44:08 +0530

    to check whether the given string is a palindrome or not

---
+++ palindrome_string/palindrome_string.rb
@@ -0,0 +1,11 @@
+puts "enter the string to check for palindrome "
+ a= ""
+a  = gets.chomp
+b = ""
+b = a.reverse
+if a==b
+ puts "the given string is a palindrome"
+ else
+puts "the given string is not a palindrome"
+end
+
